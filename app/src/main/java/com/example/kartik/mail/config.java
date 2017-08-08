fbaaeb2dd9ba3ef5a1c9814c246cdc76ff46a582
Atul Anand <atul.dc@gmail.com>
2017-08-08 15:35:55 +0530

    mail
    mail

---
+++ app/src/main/java/com/example/kartik/mail/config.java
@@ -0,0 +1,10 @@
+package com.example.kartik.mail;
+
+/**
+ * Created by Kartik on 8/7/2017.
+ */
+
+    public class config {
+        public static final String EMAIL ="jatinvijay7827@gmail.com";
+        public static final String PASSWORD ="jatin1234";
+}
